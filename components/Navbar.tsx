4c24688a7e829d4f256b7ca137d3f98447a53d90
kwasi <affulnana04@gmail.com>
2025-05-20 13:38:02 +0000

    add navbar to it

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,157 +6,101 @@ import { usePathname } from "next/navigation";
 import { Menu, X } from "lucide-react";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+export const navLinks = [
+  { label: "Home", href: "/" },
+  { label: "About Us", href: "/#about" },
+  { label: "Services", href: "/#services" },
+  { label: "Products", href: "/#products" },
+  { label: "Contact", href: "/#contact" },
+];
 
 const Navbar: React.FC = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const pathname = usePathname();
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  // Function to determine if a link is active
-  const isActive = (path: string) => {
-    if (path === "/" && pathname === "/") return true;
-    if (path !== "/" && pathname.startsWith(path)) return true;
-    return false;
-  };
-
-  // Get link classes based on active state
-  const getLinkClasses = (path: string) => {
-    return isActive(path)
-      ? "text-kworld-accent border-b-2 border-kworld-accent font-bold"
-      : "text-kworld-dark hover:text-kworld-secondary/70 transition duration-300 font-medium";
-  };
-
-  // Get mobile link classes based on active state
-  const getMobileLinkClasses = (path: string) => {
-    return isActive(path)
-      ? "block px-3 py-2 rounded-md text-base font-bold text-kworld-accent bg-gray-50 border-l-4 border-kworld-accent"
-      : "block px-3 py-2 rounded-md text-base font-medium text-kworld-dark hover:text-kworld-primary hover:bg-gray-100";
-  };
+  const logo = "/assets/Datasol_logo.png";
 
   return (
     <nav className="bg-white shadow-sm sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-20">
+        <div className="flex items-center justify-between h-25 py-4">
           <div className="flex items-center">
             <Link href="/" className="flex-shrink-0 flex items-center">
               <Image
-                src="/assets/Datasol_logo.png"
+                src={logo}
                 alt="logo"
                 width={50}
-                height={80}
+                height={100}
+                className="h-20 w-auto"
               />
-              {/* <span className="text-2xl font-bold text-kworld-primary">
-                Data
-              </span>
-              <span className="text-kworld-accent font-medium">Network</span> */}
             </Link>
           </div>
 
           {/* Desktop Navigation */}
-          <div className="hidden md:flex items-center space-x-8">
-            <Link href="/" className={getLinkClasses("/")}>
-              Home
-            </Link>
-            <Link href="/about" className={getLinkClasses("/about")}>
-              About
-            </Link>
-            <Link href="/services" className={getLinkClasses("/services")}>
-              Services
-            </Link>
-            <Link
-              href="/case-studies"
-              className={getLinkClasses("/case-studies")}
-            >
-              Case Studies
-            </Link>
-            <Link href="/faq" className={getLinkClasses("/faq")}>
-              FAQ
-            </Link>
-          </div>
+          <nav className="hidden md:flex space-x-8 text-base font-normal">
+            {navLinks.map((link) => (
+              <a
+                key={link.label}
+                href={link.href}
+                className={`hover:text-kworld-accent transition duration-300`}
+              >
+                {link.label}
+              </a>
+            ))}
+          </nav>
 
           <div className="hidden md:flex items-center space-x-8">
-            <Link href="/contact">
-              <Button
-                variant="default"
-                className={`${
-                  isActive("/contact")
-                    ? "bg-kworld-accent"
-                    : "bg-kworld-primary"
-                } hover:bg-kworld-primary/90`}
-              >
-                Contact Us
-              </Button>
-            </Link>
+            <a
+              href="/#contact"
+              className="text-white capitalize bg-brand-primary py-4 px-7 rounded-lg"
+            >
+              Get a repair now
+            </a>
           </div>
 
           {/* Mobile menu button */}
-          <div className="md:hidden flex items-center">
-            <button
-              onClick={toggleMenu}
-              className="inline-flex items-center justify-center p-2 rounded-md text-kworld-dark hover:text-kworld-primary hover:bg-gray-100 focus:outline-none"
-            >
-              {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
-            </button>
+          <div className="md:hidden">
+            <Sheet>
+              <SheetTrigger>
+                <Menu className="h-6 w-6 " />
+              </SheetTrigger>
+              <SheetContent side="left" className="w-1/2 min-w-64 p-6 bg-white">
+                <div className="mb-4">
+                  <Image
+                    src={logo}
+                    alt="datasol Logo"
+                    width={50}
+                    height={50}
+                    className="h-16 w-auto"
+                  />
+                </div>
+                <nav className="flex flex-col space-y-4 text-gray-700 font-medium">
+                  {navLinks.map((link) => (
+                    <SheetClose asChild key={link.label}>
+                      <a
+                        href={link.href}
+                        className={`transition-colors duration-300 ${
+                          pathname === link.href
+                            ? "text-accent font-medium"
+                            : "hover:text-accent/50 font-light"
+                        }`}
+                      >
+                        {link.label}
+                      </a>
+                    </SheetClose>
+                  ))}
+                </nav>
+              </SheetContent>
+            </Sheet>
           </div>
         </div>
       </div>
-
-      {/* Mobile menu */}
-      {isMenuOpen && (
-        <div className="md:hidden bg-white h-screen border-t">
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <Link
-              href="/"
-              className={getMobileLinkClasses("/")}
-              onClick={toggleMenu}
-            >
-              Home
-            </Link>
-            <Link
-              href="/about"
-              className={getMobileLinkClasses("/about")}
-              onClick={toggleMenu}
-            >
-              About
-            </Link>
-            <Link
-              href="/services"
-              className={getMobileLinkClasses("/services")}
-              onClick={toggleMenu}
-            >
-              Services
-            </Link>
-            <Link
-              href="/case-studies"
-              className={getMobileLinkClasses("/case-studies")}
-              onClick={toggleMenu}
-            >
-              Case Studies
-            </Link>
-            <Link
-              href="/faq"
-              className={getMobileLinkClasses("/faq")}
-              onClick={toggleMenu}
-            >
-              FAQ
-            </Link>
-            <Link
-              href="/contact"
-              className={`${
-                isActive("/contact")
-                  ? "block px-3 py-2 rounded-md text-base font-bold text-white bg-kworld-accent"
-                  : "block px-3 py-2 rounded-md text-base font-medium text-white bg-kworld-primary hover:bg-kworld-primary/90"
-              }`}
-              onClick={toggleMenu}
-            >
-              Contact Us
-            </Link>
-          </div>
-        </div>
-      )}
     </nav>
   );
 };
