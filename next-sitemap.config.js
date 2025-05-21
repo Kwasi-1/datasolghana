a0910073d9ca256e865ff8304fecf8fa8af73dd2
kwasi <affulnana04@gmail.com>
2025-05-21 15:58:16 +0000

    install sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://datasolghana.vercel.app/', // ← Replace with your actual live URL
+  generateRobotsTxt: true, // Also creates robots.txt
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 7000,
+};
