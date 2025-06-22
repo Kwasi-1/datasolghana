639202bbf2d329603e23c8bd8521bce81e688e57
kwasi <affulnana04@gmail.com>
2025-06-22 00:15:26 +0000

    fix the site map generation and URL

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://datasolghana.vercel.app/', // ← Replace with your actual live URL
+  siteUrl: 'https://datasolghana.net/', // ← Replace with your actual live URL
   generateRobotsTxt: true, // Also creates robots.txt
   changefreq: 'weekly',
   priority: 0.7,
